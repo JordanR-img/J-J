09c4cd7596f85b20ce665d6a29bd5ac15d3654a2
JordanR-img <jordanrobin18@gmail.com>
2022-11-13 08:35:29 +1300

    adding css to nav buttons

--- src/components/MainContentHomePage.js
+++ src/components/MainContentHomePage.js
@@ -1,36 +1,36 @@
-import Style from '../Styling/HomePage/HomePageMainContent.module.css';
+import Style from "../Styling/HomePage/HomePageMainContent.module.css";
 
-
-export default function MainContent(){
-return (
+export default function MainContent() {
+  return (
     <div className={Style.MainContentBox}>
-        <div className={Style.BoxNavBar}>
-            <ul className={Style.NavBar}>
-                <li className={Style.ProjectsNav}><h1>Projects</h1></li>
-                <li>Social</li>
-                <li>Developers</li>
-                <li>nav 4</li>
-                <li>nav 5</li>
-            </ul>
+      <div className={Style.BoxNavBar}>
+        <ul className={Style.NavBar}>
+          <li className={Style.ProjectsNav}>
+            <h1>Projects</h1>
+          </li>
+          <li className={Style.ProjectsNav}>
+            <h1>Projects</h1>
+          </li>
+          <li className={Style.ProjectsNav}>
+            <h1>Projects</h1>
+          </li>
+          <li className={Style.ProjectsNav}>
+            <h1>Projects</h1>
+          </li>
+          <li className={Style.ProjectsNav}>
+            <h1>Projects</h1>
+          </li>
+        </ul>
+      </div>
 
-
-
+      <div className={Style.BoxProfilePic}>
+        <div className={Style.Jordan}>
+          {" "}
+          <img alt="" src="/public/ProfileTest.png" />
+          image 1{" "}
         </div>
-
-
-
-        <div className={Style.BoxProfilePic}>
-           <div className={Style.Jordan}> <img alt="" src="./public/ProfileTest.png"/>image 1 </div>
-           <div className={Style.Jone}> image 2 </div>
-        </div>
+        <div className={Style.Jone}> image 2 </div>
+      </div>
     </div>
-   
-
-
-
-
-)
-
-
-
-};
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,15 @@
-import Style from '../Styling/HomePage/HomePage.module.css';
+import Style from "../Styling/HomePage/HomePage.module.css";
 import MainContent from "../components/MainContentHomePage";
 export default function HomePage() {
   return (
-  
     <div className={Style.HomePage}>
-      
-      <div><header>header</header></div>
+      <div>
+        <header>header</header>
+      </div>
 
-        <MainContent />
-
-        
+      <MainContent />
 
       {/* <div><footer>this may contain a nav</footer></div> */}
-
-
     </div>
   );
 }

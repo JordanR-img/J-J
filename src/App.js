d940d9653fdee457dbd741b4b657701cd970b90e
JordanR-img <jordanrobin18@gmail.com>
2022-11-25 16:49:18 +1300

    hello there

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import HomePage from "../src/pages/HomePage.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 export default function App() {
@@ -7,7 +7,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route>
-          <Route path="/jordan" element={<HomePage />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
         </Route>
       </Routes>
     </Router>
